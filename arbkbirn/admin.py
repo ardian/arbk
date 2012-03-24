a774d84dc535c17767ec51098e49ef339ee4e0a8
Ardian Haxha <ardian.haxha@gmail.com>
2012-03-24 15:39:45 +0100

    add source code

---
+++ arbkbirn/admin.py
@@ -0,0 +1,7 @@
+from arbkbirn.models import Business
+from arbkbirn.models import BusinessName
+
+from django.contrib import admin
+
+admin.site.register(Business)
+admin.site.register(BusinessName)
