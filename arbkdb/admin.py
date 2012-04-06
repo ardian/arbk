07bf1b348a6c758e402f02245e127bfe5b7a1f4b
James Michael DuPont <JamesMikeDuPont@googlemail.com>
2012-04-06 20:01:57 +0200

    update and rename

---
+++ arbkdb/admin.py
@@ -0,0 +1,20 @@
+from arbkdb.models import ArbkBusinesscategory
+from arbkdb.models import ArbkCompany
+from arbkdb.models import ArbkCompanyAuthorizedpersons
+from arbkdb.models import ArbkCompanyOwners
+from arbkdb.models import ArbkCompanySecondarycategories
+from arbkdb.models import ArbkCompanycategory
+from arbkdb.models import ArbkLegalentity
+from arbkdb.models import ArbkPerson
+
+
+from django.contrib import admin
+
+admin.site.register(ArbkBusinesscategory)
+admin.site.register(ArbkCompany)
+admin.site.register(ArbkCompanyAuthorizedpersons)
+admin.site.register(ArbkCompanyOwners)
+admin.site.register(ArbkCompanySecondarycategories)
+admin.site.register(ArbkCompanycategory)
+admin.site.register(ArbkLegalentity)
+admin.site.register(ArbkPerson)
