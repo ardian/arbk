dbdcc0225ff4c75f4ecab7698f94c0d3f5026405
Ardian Haxha <ardian.haxha@gmail.com>
2012-04-22 18:54:52 +0200

    update

--- settings.py
+++ settings.py
@@ -133,7 +133,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'arbkdb',
-    'search',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
